b5bbd89f7c472a87f928b53dd1f6999363bf2863
kunalmaurya-17-24 <166983031+kunalmaurya-17-24@users.noreply.github.com>
2025-03-12 02:10:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use("/api", routes);
 
+app.get("/", (req, res) => {
+    res.send("Welcome to the Node.js School API!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     
